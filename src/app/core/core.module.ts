069ead14facb1f295ca4b8abd1b54cb1e270e25a
Angelica <angelica.arroyavem@udea.edu.co>
2019-08-21 21:23:10 -0500

    adding structure

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ]
+})
+export class CoreModule { }
