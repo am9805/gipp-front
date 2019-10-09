cf95d000157516f376b16a4f2b8015aba7bc6804
angelica <angelica@experimentality.co>
2019-10-08 23:53:23 -0500

    adding the report type asigment and impact level asigment

--- src/app/reports/manage-reports/manage-reports.component.ts
+++ src/app/reports/manage-reports/manage-reports.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReportsService } from 'src/app/core/services/reports.service';
-import {UserService} from '../../core/services/user.service'
+import { UserService } from '../../core/services/user.service'
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -16,39 +16,46 @@ export class ManageReportsComponent implements OnInit {
   public report: FormControl;
   reportId;
   constructor(public reportService: ReportsService,
-    private router: Router, 
+    private router: Router,
     private userService: UserService,
     private activatedRoute: ActivatedRoute) { }
 
-  
+
 
   ngOnInit() {
-    this.reportId = Number(this.activatedRoute.snapshot.url[2].path.toString()) ;
-    
+    this.reportId = Number(this.activatedRoute.snapshot.url[2].path.toString());
+
     this.rol = localStorage.getItem('userRol');
     this.reportForm = new FormGroup({
-      'responsibleId': new FormControl('', ),
-      'investigatorId': new FormControl('',),
+      'responsibleId': new FormControl(''),
+      'investigatorId': new FormControl(''),
+      'impactLevel': new FormControl(''),
+      'reportType': new FormControl(''),
     });
     this.getUsers();
   }
-  getUsers(){
+  getUsers() {
     this.userService.getUsers().subscribe(res => {
       this.users = res;
     });
   }
 
-  send(){
-    let reportInfo = {
+  send() {
+    const reportInfo = {
       'reportid': this.reportId,
       'responsibleid': Number(this.reportForm.get('responsibleId').value),
       'investigatorid': Number(this.reportForm.get('investigatorId').value),
+      'impactLevel':  Number(this.reportForm.get('impactLevel').value),
+      'reportType': this.reportForm.get('reportType').value,
     };
-    this.reportService.updateReport(reportInfo).subscribe(response => {
-      if(response){
-        alert(response["message"])
-        this.router.navigate(['/inicio']);
-      }
-    });
+
+    console.log(reportInfo);
+    
+    // this.reportService.updateReport(reportInfo).subscribe(response => {
+    //   if (response) {
+    //     alert(response['message']);
+    //     this.router.navigate(['/inicio']);
+    //   }
+    // });
   }
 }
